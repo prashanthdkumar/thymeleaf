85d9969e330379c6463527b7d1c9690fc15b3b62
Prashanth <prashanthdkumar88@gmail.com>
2022-05-10 00:12:17 -0400

    Add Employee functionality

--- src/main/java/com/example/springbootwebapp/controller/EmployeeController.java
+++ src/main/java/com/example/springbootwebapp/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import com.example.springbootwebapp.entity.Employee;
 import com.example.springbootwebapp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,11 +17,26 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
     
-    @GetMapping({"/showEmployees", "/", "list"})
+    @GetMapping({"/showEmployees", "/", "/list"})
     public ModelAndView showEmployees() {
         ModelAndView modelAndView  = new ModelAndView("list-employees");
         List<Employee> employeeList = employeeRepository.findAll();
         modelAndView.addObject("employees", employeeList);
         return modelAndView;
     }
+    
+    @GetMapping("/addEmployeeForm")
+    public ModelAndView addEmployeeForm() {
+        ModelAndView modelAndView  = new ModelAndView("add-employee-form");
+        Employee newEmployee = new Employee();
+        modelAndView.addObject("employee", newEmployee);
+        return modelAndView;
+    }
+    
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute Employee employee) {
+        employeeRepository.save(employee);
+        return "redirect:/list";
+    }
+    
 }
